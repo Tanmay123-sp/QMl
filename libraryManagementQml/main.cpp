68e345aa1141716467a4d3f37c973427e6890b90
Tanmay123-sp <83634606+Tanmay123-sp@users.noreply.github.com>
2025-04-05 15:56:58 +0530

    Update main.cpp

--- libraryManagementQml/main.cpp
+++ libraryManagementQml/main.cpp
@@ -7,25 +7,16 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    // qmlRegisterType<DatabaseManager>("MyModule", 1, 0, "DatabaseManager");
+ 
     qmlRegisterType<MainWindow>("MyModule", 1, 0, "MainWindow");
 
     QQmlApplicationEngine engine;
-    // Api api;
+
     MainWindow mainWindow;
 
     // Expose the MainWindow instance to QML
     engine.rootContext()->setContextProperty("mainWindow", &mainWindow);
 
-    // engine.load(QUrl(QStringLiteral("C:/Users/HPGGS03/Documents/libraryManagementQml/main.cpp")));
-    // if (engine.rootObjects().isEmpty())
-        // return -1;
-
-    // engine.rootContext()->setContextProperty("mainwindow", &w);
-
-    // engine.rootContext()->setContextProperty("api", &api);
-
-    // engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
